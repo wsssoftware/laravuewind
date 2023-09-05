31dfa56a53b9e8386b2c29296a58bfe3552535d5
allanmcarvalho <allanmcarvalho@users.noreply.github.com>
2023-09-05 13:31:20 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,8 @@
 namespace Laravuewind\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Orchestra\Testbench\TestCase as Orchestra;
 use Laravuewind\LaravuewindServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
