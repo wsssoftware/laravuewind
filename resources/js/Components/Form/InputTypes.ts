b6b0a4b267d149eb6a758617ea54271afbfa2851
Allan Mariucci Carvalho <allan.m.carvalho@outlook.com>
2023-09-25 15:46:47 -0300

    wip

--- resources/js/Components/Form/InputTypes.ts
+++ resources/js/Components/Form/InputTypes.ts
@@ -1,7 +1,8 @@
 import {Component} from "vue";
 import {FilePondOptions} from "filepond";
 
-export type SelectChoice = {key: string, value: string, extra?: object};
+
+export type SelectChoice = {key: string, value: string|number|boolean, extra?: object};
 
 export type SelectOptions = {
     clearable?: boolean,
@@ -12,7 +13,55 @@ export type SelectOptions = {
 };
 
 export type FilePondParams = {
-    options?: FilePondOptions,
+    options?: FilePondOptions&{
+        allowImageValidateSize?: boolean;
+        imageValidateSizeMinWidth?: number;
+        imageValidateSizeMaxWidth?: number;
+        imageValidateSizeMinHeight?: number;
+        imageValidateSizeMaxHeight?: number;
+        imageValidateSizeLabelFormatError?: string;
+        imageValidateSizeLabelImageSizeTooSmall?: string;
+        imageValidateSizeLabelImageSizeTooBig?: string;
+        imageValidateSizeLabelExpectedMinSize?: string;
+        imageValidateSizeLabelExpectedMaxSize?: string;
+        imageValidateSizeMinResolution?: number;
+        imageValidateSizeMaxResolution?: number;
+        imageValidateSizeLabelImageResolutionTooLow?: string;
+        imageValidateSizeLabelImageResolutionTooHigh?: string;
+        imageValidateSizeLabelExpectedMinResolution?: string;
+        imageValidateSizeLabelExpectedMaxResolution?: string;
+        imageValidateSizeMeasure?: (
+            file: File
+        ) => Promise<{ width: number; height: number }>;
+    }&{
+        allowImagePreview?: boolean;
+        imagePreviewMinHeight?: number;
+        imagePreviewMaxHeight?: number;
+        imagePreviewHeight?: number;
+        imagePreviewMaxFileSize?: string;
+        imagePreviewTransparencyIndicator?: string;
+        imagePreviewMaxInstantPreviewFileSize?: number;
+        imagePreviewMarkupShow?: boolean;
+        imagePreviewMarkupFilter?: (markupItem: any) => true;
+    }&{
+        allowFileTypeValidation?: boolean;
+        acceptedFileTypes?: string[];
+        labelFileTypeNotAllowed?: string;
+        fileValidateTypeLabelExpectedTypes?: string;
+        fileValidateTypeLabelExpectedTypesMap?: object;
+        fileValidateTypeDetectType?: (file: globalThis.File, type: string) => Promise<string>;
+    }&{
+        allowFileSizeValidation?: boolean;
+        minFileSize?: string | null;
+        labelMinFileSizeExceeded?: string;
+        labelMinFileSize?: string;
+        maxFileSize?: string | null;
+        maxTotalFileSize?: string | null;
+        labelMaxFileSizeExceeded?: string;
+        labelMaxFileSize?: string;
+        labelMaxTotalFileSizeExceeded?: string;
+        labelMaxTotalFileSize?: string;
+    },
     plugins?: any[],
 };
 
--- resources/js/index.ts
+++ resources/js/index.ts
@@ -1,9 +1,11 @@
 import plugin from "./plugin";
 
+export {default as FilePondInput} from "./Components/Form/FilePondInput.vue";
 export {default as FillableInput} from "./Components/Form/FillableInput.vue";
 export {default as InputGroup} from "./Components/Form/InputGroup.vue";
 export {default as InputHelp} from "./Components/Form/InputFeedback.vue";
 export {default as InputLabel} from "./Components/Form/InputLabel.vue";
 export {default as SelectableInput} from "./Components/Form/SelectableInput.vue";
+export * from "./Components/Form/InputTypes";
 
 export default plugin;
