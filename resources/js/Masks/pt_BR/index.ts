31acd18ea5d847562fc67cd315ec33b8c7a668d3
Allan Mariucci Carvalho <allan.m.carvalho@outlook.com>
2023-09-28 17:28:36 -0300

    wip

---
+++ resources/js/Masks/pt_BR/index.ts
@@ -0,0 +1,6 @@
+export {default as cpf} from './cep'
+export {default as document} from './document'
+export {default as grams} from './grams'
+export {default as millimeters} from './millimeters'
+export {default as percentage} from './percentage'
+export {default as phone} from './phone'
