e1d9f45fb1f18072bf1bb73c1f10da38e7da3742
allanmcarvalho <allanmcarvalho@users.noreply.github.com>
2023-09-28 20:25:54 +0000

    Fix styling

--- src/Mask/pt_BR/Document.php
+++ src/Mask/pt_BR/Document.php
@@ -7,13 +7,13 @@ use Laravuewind\Support\MaskRecipe;
 
 class Document extends MaskRecipe
 {
-
     public function getRegexPattern(string $value): array|Collection
     {
         $value = preg_replace('/[^0-9]/', '', $value);
         if (strlen($value) === 14) {
-            return  ['/\d/', '/\d/', '.', '/\d/', '/\d/', '/\d/', '.', '/\d/', '/\d/', '/\d/', '/', '/\d/', '/\d/', '/\d/', '/\d/', '-', '/\d/', '/\d/'];
+            return ['/\d/', '/\d/', '.', '/\d/', '/\d/', '/\d/', '.', '/\d/', '/\d/', '/\d/', '/', '/\d/', '/\d/', '/\d/', '/\d/', '-', '/\d/', '/\d/'];
         }
+
         return ['/\d/', '/\d/', '/\d/', '.', '/\d/', '/\d/', '/\d/', '.', '/\d/', '/\d/', '/\d/', '-', '/\d/', '/\d/'];
 
     }
--- src/Mask/pt_BR/Phone.php
+++ src/Mask/pt_BR/Phone.php
@@ -7,23 +7,23 @@ use Laravuewind\Support\MaskRecipe;
 
 class Phone extends MaskRecipe
 {
-
     public function getRegexPattern(string $value): array|Collection
     {
         $value = preg_replace('/[^0-9]/', '', $value);
 
-        if (preg_match('/^[1-9][0-9]9$/', substr($value,  0, 3)) === 1) {
+        if (preg_match('/^[1-9][0-9]9$/', substr($value, 0, 3)) === 1) {
             return ['/\d/', '/\d/', ' ', '/\d/', ' ', '/\d/', '/\d/', '/\d/', '/\d/', '-', '/\d/', '/\d/', '/\d/', '/\d/'];
         }
-        if (preg_match('/^400$/', substr($value,  0, 3)) === 1) {
+        if (preg_match('/^400$/', substr($value, 0, 3)) === 1) {
             return ['/\d/', '/\d/', '/\d/', '/\d/', '-', '/\d/', '/\d/', '/\d/', '/\d/'];
         }
-        if (preg_match('/^0[3589]00$/', substr($value,  0, 4)) === 1) {
+        if (preg_match('/^0[3589]00$/', substr($value, 0, 4)) === 1) {
             return ['/\d/', '/\d/', '/\d/', '/\d/', ' ', '/\d/', '/\d/', '/\d/', ' ', '/\d/', '/\d/', '/\d/', '/\d/'];
         }
-        if (preg_match('/^[1-9][0-9][1-5][0-9]$/', substr($value,  0, 4)) === 1) {
+        if (preg_match('/^[1-9][0-9][1-5][0-9]$/', substr($value, 0, 4)) === 1) {
             return ['/\d/', '/\d/', ' ', '/\d/', '/\d/', '/\d/', '/\d/', '-', '/\d/', '/\d/', '/\d/', '/\d/'];
         }
+
         return ['/\d/', '/\d/', '/\d/'];
     }
 }
