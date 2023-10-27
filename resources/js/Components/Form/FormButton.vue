1addb97a0605141240390d4299849604ef9d9edb
Allan Mariucci Carvalho <allan.m.carvalho@outlook.com>
2023-10-27 14:50:41 -0300

    wip

---
+++ resources/js/Components/Form/FormButton.vue
@@ -0,0 +1,19 @@
+<template>
+    <button class="disabled:opacity-75" :disabled="form?.processing"></button>
+</template>
+
+<script lang="ts">
+import {defineComponent, PropType} from "vue";
+import {InertiaForm} from "@inertiajs/vue3/types/useForm";
+
+export default defineComponent({
+    name: "FormButton",
+    props: {
+        form: {type: Object as PropType<InertiaForm<object>>, required: true},
+    }
+});
+</script>
+
+<style scoped>
+
+</style>
--- resources/js/index.ts
+++ resources/js/index.ts
@@ -4,11 +4,13 @@ export {default as CheckboxGroup} from "./Components/Form/CheckboxGroup.vue";
 export {default as Datetime} from "./Components/Datetime.vue";
 export {default as FilePondInput} from "./Components/Form/FilePondInput.vue";
 export {default as FillableInput} from "./Components/Form/FillableInput.vue";
+export {default as FormButton} from "./Components/Form/FormButton.vue";
 export {default as Image} from "./Components/Image.vue";
 export {default as InputGroup} from "./Components/Form/InputGroup.vue";
 export {default as InputHelp} from "./Components/Form/InputFeedback.vue";
 export {default as InputLabel} from "./Components/Form/InputLabel.vue";
 export {default as SelectableInput} from "./Components/Form/SelectableInput.vue";
+export {default as Tooltip} from "./Directives/Tooltip";
 export * from "./Support/Number";
 export * from "./Components/Form/InputTypes";
 
