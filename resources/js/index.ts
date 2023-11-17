0e824e1edbcbd78d07ba2a4c6d2a1f253303780f
Allan Mariucci Carvalho <allan.m.carvalho@outlook.com>
2023-11-17 12:35:14 -0300

    wip

--- resources/js/index.ts
+++ resources/js/index.ts
@@ -11,7 +11,6 @@ export {default as InputGroup} from "./Components/Form/InputGroup.vue";
 export {default as InputHelp} from "./Components/Form/InputFeedback.vue";
 export {default as InputLabel} from "./Components/Form/InputLabel.vue";
 export {default as SelectableInput} from "./Components/Form/SelectableInput.vue";
-export {default as VModelSelectableInput} from "./Components/Form/VModelSelectableInput.vue";
 export {default as Tooltip} from "./Directives/Tooltip";
 export * from "./Support/Number";
 export * from "./Components/Form/InputTypes";
